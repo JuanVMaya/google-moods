2f05d8308aee6f0faca1c0919e119893cfe02763
Muhammad Choudhary <choudha1@sheridancollege.ca>
2022-07-29 12:36:50 -0400

    Complete feelingBox component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-import Homepage from "./pages/HomePage/HomePage";
+import Homepage from "./Pages/HomePage/HomePage";
 import "./App.scss";
+import "./components/feelingBox/FeelingBox";
+import feelingBox from "./components/feelingBox/FeelingBox";
 
 function App() {
   return (
     <Router>
       <Switch>
-        <Route path="/" exact component={Homepage}>
+        <Route path="/" exact component={feelingBox}>
           
         </Route>
       </Switch>
