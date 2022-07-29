bb0e136e42228dba1a55e38fff02754b237b7586
chelsia tong <chelsiatong@gmail.com>
2022-07-28 21:04:12 -0400

    created folders and files

--- src/App.js
+++ src/App.js
@@ -1,32 +1,17 @@
-import logo from "./logo.svg";
-import { useState } from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import "./App.css";
+import Homepage from '';
 
 function App() {
-  const [count, setCount] = useState(0);
-  const handleCountUp = () => {
-    setCount(count - 1);
-  };
-
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <p>This is the count down! {count}</p>
-        <button onClick={handleCountUp}>Count down!</button>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+
+    <Router>
+      <Switch>
+
+        <Route path="/" exact component={HomePage}> </Route>
+      </Switch>
+    </Router>
+
   );
 }
 
