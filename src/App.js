4fbc821a36df6ebac354039033540fa27181249e
Muhammad Choudhary <choudha1@sheridancollege.ca>
2022-07-28 22:15:02 -0400

    Complete basic version of sign in page

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import logo from "./logo.svg";
 import { useState } from "react";
 import "./App.css";
+import { BrowserRouter, Switch, Route, useHistory  } from "react-router-dom";
+import LoginPage from "./Pages/SignInPage/SignInPage";
 
 function App() {
   const [count, setCount] = useState(0);
@@ -8,26 +10,39 @@ function App() {
     setCount(count - 1);
   };
 
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <p>This is the count down! {count}</p>
-        <button onClick={handleCountUp}>Count down!</button>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Switch>
+        <Route
+          path="/login" exact component={LoginPage}
+        />
+
+
+      </Switch>
+    </BrowserRouter>
   );
+
+  // return (
+  //   <div className="App">
+  //     <header className="App-header">
+  //       <img src={logo} className="App-logo" alt="logo" />
+  //       <p>
+  //         Edit <code>src/App.js</code> and save to reload.
+  //       </p>
+  //       <p>This is the count down! {count}</p>
+  //       <button onClick={handleCountUp}>Count down!</button>
+  //       <a
+  //         className="App-link"
+  //         href="https://reactjs.org"
+  //         target="_blank"
+  //         rel="noopener noreferrer"
+  //       >
+  //         Learn React
+  //       </a>
+  //     </header>
+  //   </div>
+  // );
 }
 
 export default App;
