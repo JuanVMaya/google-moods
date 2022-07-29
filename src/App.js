f4f0cd958493a2760c2c938f3247449015530247
chelsia tong <chelsiatong@gmail.com>
2022-07-29 17:24:14 -0400

    test

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Homepage from "./pages/HomePage/HomePage";
-// import SignInPage from "./pages/SignInPage/SignInPage";
 import "./App.scss";
 
 function App() {
