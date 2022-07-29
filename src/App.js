4f09b2cb288e9216ac91fe05b811837dc4f725a0
Muhammad Choudhary <choudha1@sheridancollege.ca>
2022-07-29 12:59:34 -0400

    Merge develop into feature/feelingBox

--- src/App.js
+++ src/App.js
@@ -1,8 +1,7 @@
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Homepage from "./Pages/HomePage/HomePage";
 import "./App.scss";
-import "./components/FeelingBox/FeelingBox";
-import FeelingBox from "./components/FeelingBox/FeelingBox";
+
 
 function App() {
   return (
